f28ca95d24dc03b4788a7ed83067f0cf29ab061c
Haz <abdulcodesofficial@gmail.com>
2025-01-19 13:02:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed("#typed", {
+  strings: [
+    "<b>Programming</b>",
+    "<b>Typing</b>",
+    "<b>Chess</b>",
+    "<b>Music</b>",
+    "<b>Internet Surfing</b>",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+  showCursor: false,
+});
